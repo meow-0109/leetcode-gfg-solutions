3c73085c8f63efe661fb18904aa328b69dbae633
Rutuja Soundale <rutujasoundale693@gmail.com>
2025-10-08 19:26:10 +0530

    Create reverse_string.java

---
+++ reverse_string.java
@@ -0,0 +1,18 @@
+//Using swapping O(n) time complexity
+//O(1) space complexity
+class Solution {
+    public void reverseString(char[] s) {
+        
+        int left=0;
+        int right=s.length-1;
+        while(left<right){
+            char temp=s[left];
+            s[left]=s[right];
+            s[right]=temp;
+            left++;
+            right--;
+        }
+        
+
+    }
+}
