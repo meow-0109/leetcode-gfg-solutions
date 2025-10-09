d8972915dcd49b490be0042519e97fba6150a185
Rutuja Soundale <rutujasoundale693@gmail.com>
2025-10-10 01:23:08 +0530

    Create shufflestring.java

---
+++ shufflestring.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+     char[] S=s.toCharArray();
+     char[] t=new char[indices.length];
+      for(int i=0;i<S.length;i++ ){
+        t[i]=S[i];
+        
+      } 
+      return s;
+    }
+}
